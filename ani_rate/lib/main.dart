c430ff96ad0a485f1c11afee7850eabac3d75225
Kemilab <lukasalaj9@gmail.com>
2024-04-22 18:37:25 +0200

    Updated the ui

--- ani_rate/lib/main.dart
+++ ani_rate/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
   }
--- ani_rate/lib/pages/splash.dart
+++ ani_rate/lib/pages/splash.dart
@@ -1,6 +1,9 @@
 import 'package:ani_rate/pages/home.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -10,18 +13,29 @@ class SplashScreen extends StatelessWidget {
     return AnimatedSplashScreen(
       splash: Column(
         children: [
-          Image.asset('assets/logo2.png'),
-          const Text(
+          SvgPicture.asset(
+            "assets/logo.svg",
+            // height: 70.0,
+            //width: 70.0,
+          ),
+          SvgPicture.asset(
+            "assets/title.svg",
+            height: 200.0,
+            width: 200.0,
+          ),
+          /*const Text(
             "AniRate",
             style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-          )
+          )*/
         ],
       ),
-      splashTransition: SplashTransition.scaleTransition,
-      splashIconSize: 150,
+      splashIconSize: 350,
       duration: 1000,
       nextScreen: const MyHomeScreen(),
-      pageTransitionType: PageTransitionsType,
+      splashTransition: SplashTransition.scaleTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: const Duration(milliseconds: 1000),
+      backgroundColor: const Color.fromARGB(221, 179, 247, 52),
     );
   }
 }
