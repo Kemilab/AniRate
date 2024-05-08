9622149366973bd5f50063b332c969a9afe38785
Kemilab <lukasalaj9@gmail.com>
2024-05-08 18:40:35 +0200

    Added profile page

--- ani_rate/lib/pages/home.dart
+++ ani_rate/lib/pages/home.dart
@@ -6,159 +6,167 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomeScreen extends StatefulWidget {
- MyHomeScreen({super.key});
+  MyHomeScreen({super.key});
 
- @override
- State<MyHomeScreen> createState() => _MyHomeScreenState();
+  @override
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
 }
 
 class _MyHomeScreenState extends State<MyHomeScreen> {
- final user = FirebaseAuth.instance.currentUser!;
+  final user = FirebaseAuth.instance.currentUser!;
 
- // Sign user out
- void signUserOut() {
-  FirebaseAuth.instance.signOut();
- }
+  // Sign user out
+  void signUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
- final TextEditingController searchController = TextEditingController();
+  final TextEditingController searchController = TextEditingController();
 
- List<top_rating_model> topRatings = [];
- List<SpotlightModel> spotlights = [];
+  List<top_rating_model> topRatings = [];
+  List<SpotlightModel> spotlights = [];
 
- void _getTopRatings() {
-  topRatings = top_rating_model.getTopRatings();
- }
- void _getSpotlights() {
-  spotlights = SpotlightModel.getSpotlights();
- }
+  void _getTopRatings() {
+    topRatings = top_rating_model.getTopRatings();
+  }
 
- @override
- void initState() {
-  _getTopRatings();
-  _getSpotlights();
- }
+  void _getSpotlights() {
+    spotlights = SpotlightModel.getSpotlights();
+  }
 
- @override
- Widget build(BuildContext context) {
-  _getTopRatings();
-  _getSpotlights();
-  return Scaffold(
-   backgroundColor: Color.fromRGBO(35, 35, 35, 1),
-   appBar: appBar(),
-   body:Stack(
-    children: [
-    Positioned(
-      top: 0.0, // Top position set to 0.0
-      left: 0.0, // Left position set to 0.0
-      right: 0.0, // Right position set to 0.0 (stretch to full width)
-      height: 200.0, // Adjust height as needed
-      child: SvgPicture.asset(
-        'assets/aot_spotlight.svg',
-        fit: BoxFit.cover,
-      ),),
-   Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-     SizedBox(
-      height: 250,
-     ),
-     Column(
-      children: [
-       Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Text(
-         'Top ratings',
-         style: TextStyle(
-           color: Color.fromARGB(255, 255, 119, 29),
-         fontSize: 18,
-           fontWeight: FontWeight.w600),
-        ),
-       ),
-       SizedBox(
-        height: 15,
-       ),
-       Container(
-         height: 250,
-         child: ListView.separated(
-          itemCount: topRatings.length,
-          scrollDirection: Axis.horizontal,
-          padding: EdgeInsets.only(left: 20, right: 20),
-          separatorBuilder: (context, index) => SizedBox(
-           width: 5,
+  @override
+  void initState() {
+    _getTopRatings();
+    _getSpotlights();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getTopRatings();
+    _getSpotlights();
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
+      appBar: appBar(),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0.0, // Top position set to 0.0
+            left: 0.0, // Left position set to 0.0
+            right: 0.0, // Right position set to 0.0 (stretch to full width)
+            height: 200.0, // Adjust height as needed
+            child: SvgPicture.asset(
+              'assets/aot_spotlight.svg',
+              fit: BoxFit.cover,
+            ),
           ),
-          itemBuilder: (context, index) {
-           return Container(
-             width: 166,
-             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-               ClipRRect( // Wrap the image with ClipRRect
-                borderRadius: BorderRadius.circular(10.0),
-                child: Image.asset(
-                 topRatings[index].coverPath,
-                 width: 200, // Adjust width if needed
-                 height: 250, // Adjust height if needed
-                 fit: BoxFit.cover,
-                ),
-                
-               ),
-              ],
-             ));
-          },
-         ))
-      ],
-     )
-    ],
-   ),
-  ],),
-  );
- }
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 250,
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      'Top ratings',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 119, 29),
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                      height: 250,
+                      child: ListView.separated(
+                        itemCount: topRatings.length,
+                        scrollDirection: Axis.horizontal,
+                        padding: EdgeInsets.only(left: 20, right: 20),
+                        separatorBuilder: (context, index) => SizedBox(
+                          width: 5,
+                        ),
+                        itemBuilder: (context, index) {
+                          return Container(
+                              width: 166,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  ClipRRect(
+                                    // Wrap the image with ClipRRect
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Image.asset(
+                                      topRatings[index].coverPath,
+                                      width: 200, // Adjust width if needed
+                                      height: 250, // Adjust height if needed
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ],
+                              ));
+                        },
+                      ))
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
- AppBar appBar() {
-  return AppBar(
-   title: TextField(
-    controller: searchController,
-    decoration: InputDecoration(
-     hintText: 'Search anime...',
-     hintStyle: TextStyle(color: Color.fromARGB(255, 255, 119, 29)),
-     prefixIcon: Icon(Icons.search),
-     enabledBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(10.0), // Add rounded corners
-      borderSide: BorderSide(
-       color: Color.fromARGB(
-         255, 255, 119, 29), // Adjust border color as desired
+  AppBar appBar() {
+    return AppBar(
+      title: TextField(
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Search anime...',
+          hintStyle: const TextStyle(color: Color.fromARGB(255, 255, 119, 29)),
+          prefixIcon: const Icon(Icons.search),
+          enabledBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0), // Add rounded corners
+            borderSide: const BorderSide(
+              color: Color.fromARGB(
+                  255, 255, 119, 29), // Adjust border color as desired
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0), // Add rounded corners
+            borderSide: const BorderSide(
+              color: Colors.blue, // Adjust border color for focus state
+            ),
+          ),
+        ),
       ),
-     ),
-     focusedBorder: UnderlineInputBorder(
-      borderRadius: BorderRadius.circular(10.0), // Add rounded corners
-      borderSide: BorderSide(
-       color: Colors.blue, // Adjust border color for focus state
+      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
+      leading: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: const Color.fromRGBO(35, 35, 35, 1),
+            borderRadius: BorderRadius.circular(10)),
+        child: GestureDetector(
+          onTap: () {
+            showModalBottomSheet(
+                useSafeArea: true,
+                enableDrag: true,
+                context: context,
+                builder: ((context) {
+                  return ProfilePage();
+                }));
+          },
+          child: Image.asset(
+            'assets/logo.png',
+            height: 50,
+            width: 50,
+          ),
+        ),
       ),
-     ),
-    ),
-   ),
-   backgroundColor: Color.fromRGBO(35, 35, 35, 1),
-   leading: Container(
-    margin: EdgeInsets.all(10),
-    alignment: Alignment.center,
-    child: GestureDetector(
-     onTap: () {
-      Navigator.push(context,
-        MaterialPageRoute(builder: (context) => ProfilePage()));
-     },
-     child: Image.asset(
-      'assets/logo.png',
-      height: 50,
-      width: 50,
-     ),
-    ),
-    decoration: BoxDecoration(
-      color: Color.fromRGBO(35, 35, 35, 1),
-      borderRadius: BorderRadius.circular(10)),
-   ),
-   actions: [
-    IconButton(onPressed: signUserOut, icon: Icon(Icons.logout)),
-   ],
-  );
- }
+      actions: [
+        IconButton(onPressed: signUserOut, icon: Icon(Icons.logout)),
+      ],
+    );
+  }
 }
--- ani_rate/lib/pages/profile_page.dart
+++ ani_rate/lib/pages/profile_page.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  final username = TextEditingController();
+  ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
+      body: SafeArea(
+        child: SizedBox(
+            height: double.infinity,
+            child: SingleChildScrollView(
+                child: Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      "Account",
+                      style: GoogleFonts.poppins(
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700)),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10.0),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.account_circle,
+                          size: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ))),
+      ),
+    );
   }
 }
