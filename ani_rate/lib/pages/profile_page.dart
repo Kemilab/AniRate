8eb8fd823d17a4195b6e5a85beb86501468a0e9f
Kemilab <lukasalaj9@gmail.com>
2024-05-08 19:47:30 +0200

    Update profile_page.dart

--- ani_rate/lib/pages/profile_page.dart
+++ ani_rate/lib/pages/profile_page.dart
@@ -1,55 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
-  final username = TextEditingController();
   ProfilePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Color.fromRGBO(35, 35, 35, 1),
-      body: SafeArea(
-        child: SizedBox(
-            height: double.infinity,
-            child: SingleChildScrollView(
-                child: Padding(
-              padding: EdgeInsets.only(top: 15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Text(
-                      "Account",
-                      style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w700)),
-                    ),
+      body: Stack(
+        children: [
+          Positioned(
+              top: 5.0, // Top position set to 0.0
+              left: 0.0, // Left position set to 0.0
+              right: 0.0, // Right position set to 0.0 (stretch to full width)
+              height: 18.0, // Adjust height as needed
+              child: Center(
+                child: Text(
+                  "Account",
+                  style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  )),
+                ),
+              )),
+          Positioned(
+            top: 80.0,
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  margin: EdgeInsets.only(top: 5, left: 25, right: 25),
+                  padding: EdgeInsets.all(25),
+                  child: const Row(
+                    children: [],
                   ),
-                  const SizedBox(height: 10),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Icon(
-                          Icons.account_circle,
-                          size: 30,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ))),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
