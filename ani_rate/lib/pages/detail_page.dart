cf510555115805b96dd4f9ffae98fcf9ff005373
Kemilab <lukasalaj9@gmail.com>
2024-05-13 19:06:42 +0200

    Update detail_page.dart

--- ani_rate/lib/pages/detail_page.dart
+++ ani_rate/lib/pages/detail_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-class DetailPage extends StatelessWidget {
+
+class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
 
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
 
+class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Scaffold(
-        backgroundColor: Color.fromRGBO(35, 35, 35, 1),
-        body: Stack(
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(35, 35, 35, 1),
+      body: SafeArea(
+        child: Stack(
           children: [
             Positioned(
               top: 0.0, // Top position set to 0.0
