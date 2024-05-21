719676598f5da5f7fb05bada88df8428f9cb816f
JUST_Nic3 <lukakotorac05@gmail.com>
2024-05-21 17:16:55 +0200

    minor changes

--- ani_rate/lib/pages/AnimeListByTagPage.dart
+++ ani_rate/lib/pages/AnimeListByTagPage.dart
@@ -1,8 +1,8 @@
-import 'package:ani_rate/pages/anime_detail_page.dart';
-import 'package:ani_rate/providers/anime_provider.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ani_rate/pages/anime_detail_page.dart';
+import 'package:ani_rate/providers/anime_provider.dart';
 
 class AnimeListByTagPage extends StatefulWidget {
   final String tag;
@@ -15,10 +15,13 @@ class AnimeListByTagPage extends StatefulWidget {
 
 class _AnimeListByTagPageState extends State<AnimeListByTagPage> {
   late ScrollController _scrollController;
+  bool _isLoading = false;
+  bool _isMounted = false;
 
   @override
   void initState() {
     super.initState();
+    _isMounted = true;
     _scrollController = ScrollController();
     _scrollController.addListener(_scrollListener);
     _fetchAnimeByTag();
@@ -26,44 +29,35 @@ class _AnimeListByTagPageState extends State<AnimeListByTagPage> {
 
   @override
   void dispose() {
+    _isMounted = false;
+    _scrollController.removeListener(_scrollListener);
     _scrollController.dispose();
     super.dispose();
   }
 
   void _scrollListener() {
-    if (_scrollController.offset >=
-            _scrollController.position.maxScrollExtent &&
-        !_scrollController.position.outOfRange &&
-        !_isLoading &&
-        !_reachedEnd) {
+    if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent && !_isLoading) {
       _fetchAnimeByTag();
     }
   }
 
-  bool _isLoading = false;
-  bool _reachedEnd = false;
-
   Future<void> _fetchAnimeByTag() async {
-    if (_isLoading || _reachedEnd) return;
+    if (_isLoading || !_isMounted) return;
 
     setState(() {
       _isLoading = true;
     });
 
     try {
-      await Provider.of<AnimeProvider>(context, listen: false)
-          .fetchAnimeByTag(widget.tag);
+      await Provider.of<AnimeProvider>(context, listen: false).fetchAnimeByTag(widget.tag);
     } catch (error) {
       print('Error fetching anime by tag: $error');
     } finally {
-      setState(() {
-        _isLoading = false;
-        _reachedEnd = _reachedEnd ||
-            Provider.of<AnimeProvider>(context, listen: false)
-                    .animeList
-                    .length ==
-                0;
-      });
+      if (_isMounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
     }
   }
 
@@ -76,17 +70,18 @@ class _AnimeListByTagPageState extends State<AnimeListByTagPage> {
       body: Consumer<AnimeProvider>(
         builder: (context, animeProvider, _) {
           final animeList = animeProvider.animeList;
+          final hasMore = animeProvider.hasMore;
 
           return GridView.builder(
             controller: _scrollController,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 8.0,
               mainAxisSpacing: 8.0,
             ),
-            itemCount: animeList.length + (_isLoading ? 1 : 0),
+            itemCount: animeList.length + (hasMore ? 1 : 0),
             itemBuilder: (context, index) {
-              if (index == animeList.length && _isLoading) {
+              if (index == animeList.length) {
                 return Center(child: CircularProgressIndicator());
               }
 
--- ani_rate/lib/providers/anime_provider.dart
+++ ani_rate/lib/providers/anime_provider.dart
@@ -11,9 +11,12 @@ class AnimeProvider with ChangeNotifier {
   bool get isLoading => _isLoading;
 
   int _page = 1;
+  bool _hasMore = true;
+
+  bool get hasMore => _hasMore;
 
   Future<void> fetchAnime() async {
-    if (_isLoading) return;
+    if (_isLoading || !_hasMore) return;
 
     _isLoading = true;
 
@@ -23,6 +26,9 @@ class AnimeProvider with ChangeNotifier {
       const query = '''
         query (\$page: Int, \$perPage: Int) {
           Page(page: \$page, perPage: \$perPage) {
+            pageInfo {
+              hasNextPage
+            }
             media(type: ANIME, sort: POPULARITY_DESC) {
               title {
                 romaji
@@ -58,8 +64,12 @@ class AnimeProvider with ChangeNotifier {
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
         final List<dynamic> fetchedAnime = data['data']['Page']['media'];
-        _animeList =
-            fetchedAnime.map((anime) => Anime.fromJson(anime)).toList();
+        final hasNextPage = data['data']['Page']['pageInfo']['hasNextPage'];
+        
+        _hasMore = hasNextPage;
+        _animeList.addAll(
+          fetchedAnime.map((anime) => Anime.fromJson(anime)).toList(),
+        );
         _page++;
       } else {
         throw Exception('Failed to fetch anime');
@@ -73,62 +83,82 @@ class AnimeProvider with ChangeNotifier {
   }
 
   Future<void> fetchAnimeByTag(String tag) async {
-    if (_isLoading) return;
+  if (_isLoading || !_hasMore) return;
 
-    _isLoading = true;
+  _isLoading = true;
 
-    try {
-      const String url = 'https://graphql.anilist.co';
+  try {
+    const String url = 'https://graphql.anilist.co';
 
-      const query = '''
-        query (\$page: Int, \$perPage: Int, \$tag: String) {
-          Page(page: \$page, perPage: \$perPage) {
-            media(type: ANIME, tag: \$tag, sort: POPULARITY_DESC) {
-              title {
-                romaji
-                english
-              }
-              coverImage {
-                large
-              }
-              bannerImage
-              averageScore
-              popularity
-              meanScore
-              episodes
-              description
-              type
-              duration
-              tags {
-                name
-              }
+    const query = '''
+      query (\$page: Int, \$perPage: Int, \$tag: String) {
+        Page(page: \$page, perPage: \$perPage) {
+          pageInfo {
+            hasNextPage
+          }
+          media(type: ANIME, tag: \$tag, sort: POPULARITY_DESC) {
+            title {
+              romaji
+              english
+            }
+            coverImage {
+              large
+            }
+            bannerImage
+            averageScore
+            popularity
+            meanScore
+            episodes
+            description
+            type
+            duration
+            tags {
+              name
             }
           }
         }
-      ''';
-
-      final variables = {'page': _page, 'perPage': 10, 'tag': tag};
-
-      final response = await http.post(
-        Uri.parse(url),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'query': query, 'variables': variables}),
-      );
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final List<dynamic> fetchedAnime = data['data']['Page']['media'];
-        _animeList =
-            fetchedAnime.map((anime) => Anime.fromJson(anime)).toList();
-      } else {
-        throw Exception('Failed to fetch anime by tag');
       }
-    } catch (error) {
-      print('Error fetching anime by tag: $error');
-    }
+    ''';
 
-    _isLoading = false;
-    notifyListeners();
+    final variables = {'page': _page, 'perPage': 10, 'tag': tag};
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'query': query, 'variables': variables}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List<dynamic> fetchedAnime = data['data']['Page']['media'];
+      final hasNextPage = data['data']['Page']['pageInfo']['hasNextPage'];
+      
+      _hasMore = hasNextPage;
+      
+      // Clear _animeList if fetching the first page of anime by tag
+      if (_page == 1) {
+        _animeList.clear();
+      }
+      
+      _animeList.addAll(
+        fetchedAnime.map((anime) => Anime.fromJson(anime)).toList(),
+      );
+      if (hasNextPage) {
+        _page++; // Increment page only if there are more pages to fetch
+      }
+    } else {
+      throw Exception('Failed to fetch anime by tag');
+    }
+  } catch (error) {
+    print('Error fetching anime by tag: $error');
   }
+
+  _isLoading = false;
+  notifyListeners();
 }
+
+
+
+}
+
 
